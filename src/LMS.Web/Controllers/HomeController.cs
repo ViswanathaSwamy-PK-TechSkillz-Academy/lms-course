1be65183aaf08986f4d5b732412ad56358db5e53
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-08-07 23:28:55 +0530

    Code Refactoring

--- src/LMS.Web/Controllers/HomeController.cs
+++ src/LMS.Web/Controllers/HomeController.cs
@@ -4,28 +4,29 @@ using System.Diagnostics;
 
 namespace LMS.Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(ILogger<HomeController> logger) : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        private readonly ILogger<HomeController> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
         public IActionResult Index()
         {
+            _logger.LogInformation("Home page visited at {time}", DateTime.Now);
+
             return View();
         }
 
         public IActionResult Privacy()
         {
+            _logger.LogInformation("Privacy page visited at {time}", DateTime.Now);
+
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            _logger.LogError("An error occurred at {time}", DateTime.Now);
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
