14b6b08184966260142c368fa0e111e82ac7f56a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-08-07 23:58:04 +0530

    Test Controller

--- src/LMS.Web/Controllers/HomeController.cs
+++ src/LMS.Web/Controllers/HomeController.cs
@@ -2,32 +2,31 @@ using LMS.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace LMS.Web.Controllers
+namespace LMS.Web.Controllers;
+
+public class HomeController(ILogger<HomeController> logger) : Controller
 {
-    public class HomeController(ILogger<HomeController> logger) : Controller
+    private readonly ILogger<HomeController> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+    public IActionResult Index()
     {
-        private readonly ILogger<HomeController> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _logger.LogInformation("Home page visited at {time}", DateTime.Now);
 
-        public IActionResult Index()
-        {
-            _logger.LogInformation("Home page visited at {time}", DateTime.Now);
+        return View();
+    }
 
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        _logger.LogInformation("Privacy page visited at {time}", DateTime.Now);
 
-        public IActionResult Privacy()
-        {
-            _logger.LogInformation("Privacy page visited at {time}", DateTime.Now);
+        return View();
+    }
 
-            return View();
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        _logger.LogError("An error occurred at {time}", DateTime.Now);
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            _logger.LogError("An error occurred at {time}", DateTime.Now);
-
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
---
+++ src/LMS.Web/Models/TestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LMS.Web.Models;
+
+internal class TestViewModel
+{
+    public string Name { get; set; } = string.Empty;
+
+    public DateTime DateOfBirth { get; set; }
+}
