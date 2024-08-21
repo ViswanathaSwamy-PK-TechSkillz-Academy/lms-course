29d39a328cc19b2d48fe8480d2ccc602304507b2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-08-21 10:33:28 +0530

    Code Refactoring

---
+++ src/LMS.Web/Extensions/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using LMS.Web.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Web.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+        _ = builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+        _ = builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        _ = builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        _ = builder.Services.AddControllersWithViews();
+    }
+
+}
--- src/LMS.Web/Program.cs
+++ src/LMS.Web/Program.cs
@@ -1,18 +1,9 @@
-using LMS.Web.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using LMS.Web.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+builder.ConfigureServices();
 
 var app = builder.Build();
 
