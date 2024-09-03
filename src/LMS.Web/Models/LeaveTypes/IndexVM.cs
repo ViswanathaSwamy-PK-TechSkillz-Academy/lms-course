e2712938b1fca47392e653cec8f86c5a3ac8d83b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-03 23:03:22 +0530

    Update IndexVM.cs

--- src/LMS.Web/Models/LeaveTypes/IndexVM.cs
+++ src/LMS.Web/Models/LeaveTypes/IndexVM.cs
@@ -1,5 +1,13 @@
-﻿namespace LMS.Web.Models.LeaveTypes;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Web.Models.LeaveTypes;
 
 public class IndexVM
 {
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    [Display(Name = "Maximum Allocation of Days")]
+    public int NumberOfDays { get; set; }
 }
