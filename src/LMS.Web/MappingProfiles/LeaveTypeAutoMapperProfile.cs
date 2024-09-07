42142015f54826cdcb37b88e4514f8ec04d900ff
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-07 23:42:39 +0530

    Create LeaveTypeAutoMapperProfile.cs

---
+++ src/LMS.Web/MappingProfiles/LeaveTypeAutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using LMS.Data.Entities;
+using LMS.Web.Models.LeaveTypes;
+
+namespace LMS.Web.MappingProfiles;
+
+public class LeaveTypeAutoMapperProfile : Profile
+{
+    public LeaveTypeAutoMapperProfile()
+    {
+        CreateMap<LeaveType, IndexVM>().ReverseMap();
+    }
+}
