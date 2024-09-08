e902c2e87d20d3058834bc0922dcebc31555093a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-08 16:04:02 +0530

    Custom Schema

--- src/LMS.Persistence/LMSDbContext.cs
+++ src/LMS.Persistence/LMSDbContext.cs
@@ -6,4 +6,12 @@ namespace LMS.Persistence;
 public class LMSDbContext(DbContextOptions<LMSDbContext> options) : DbContext(options)
 {
     public DbSet<LeaveType> LeaveTypes => Set<LeaveType>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Set default schema for the entire context
+        modelBuilder.HasDefaultSchema("lms");
+    }
 }
