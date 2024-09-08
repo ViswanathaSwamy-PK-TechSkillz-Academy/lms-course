e317aa195f0452d005197e1c05daab804ed7423c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-08 16:08:16 +0530

    EF Core Migrations

--- src/LMS.Persistence/Migrations/20240908103744_ChangingTheSchema.Designer.cs
+++ src/LMS.Persistence/Migrations/20240908103744_ChangingTheSchema.Designer.cs
@@ -3,6 +3,7 @@ using LMS.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LMS.Persistence.Migrations
 {
     [DbContext(typeof(LMSDbContext))]
-    partial class LMSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240908103744_ChangingTheSchema")]
+    partial class ChangingTheSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
