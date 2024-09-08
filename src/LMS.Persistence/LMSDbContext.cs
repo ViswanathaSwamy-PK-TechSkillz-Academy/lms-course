75255bd717eb6d54443a5cfdb1a63cc0a8f7703c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-08 22:06:40 +0530

    Changing Schema

--- src/LMS.Persistence/LMSDbContext.cs
+++ src/LMS.Persistence/LMSDbContext.cs
@@ -5,8 +5,6 @@ namespace LMS.Persistence;
 
 public class LMSDbContext(DbContextOptions<LMSDbContext> options) : DbContext(options)
 {
-    const string Schema = "lms";
-
     public DbSet<LeaveType> LeaveTypes => Set<LeaveType>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
