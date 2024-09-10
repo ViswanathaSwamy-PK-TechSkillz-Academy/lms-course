5daee31de531511e243bd06d466f058a79678c71
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-11 00:20:39 +0530

    Swamy/10sep work (#22)
    * Renamed the variables
    
    * Update LeaveTypesController.cs
    
    * Update LeaveTypesController.cs
    
    * Update LeaveTypesController.cs
    
    * Create LeaveTypeEditVM.cs
    
    * Added Models
    
    * Update LeaveTypeAutoMapperProfile.cs

--- src/LMS.Web/MappingProfiles/LeaveTypeAutoMapperProfile.cs
+++ src/LMS.Web/MappingProfiles/LeaveTypeAutoMapperProfile.cs
@@ -8,8 +8,12 @@ public class LeaveTypeAutoMapperProfile : Profile
 {
     public LeaveTypeAutoMapperProfile()
     {
-        _ = CreateMap<LeaveType, LeaveTypeReadOnlyVM>().ReverseMap();
+        _ = CreateMap<LeaveType, LeaveTypeReadOnlyVM>();
 
         _ = CreateMap<LeaveTypeCreateVM, LeaveType>();
+
+        _ = CreateMap<LeaveTypeEditVM, LeaveType>().ReverseMap();
     }
 }
+
+//.ForMember(dest => dest.Days, opt => opt.MapFrom(src => src.NumberOfDays));
--- src/LMS.Web/Models/LeaveTypes/LeaveTypeEditVM.cs
+++ src/LMS.Web/Models/LeaveTypes/LeaveTypeEditVM.cs
@@ -2,10 +2,14 @@
 
 namespace LMS.Web.Models.LeaveTypes;
 
-public class LeaveTypeReadOnlyVM : BaseLeaveTypeVM
+public class LeaveTypeEditVM : BaseLeaveTypeVM
 {
+    [Required]
+    [Length(4, 150, ErrorMessage = "You have violated the length requirements")]
     public string Name { get; set; } = string.Empty;
 
+    [Required]
+    [Range(1, 90)]
     [Display(Name = "Maximum Allocation of Days")]
     public int NumberOfDays { get; set; }
 }
