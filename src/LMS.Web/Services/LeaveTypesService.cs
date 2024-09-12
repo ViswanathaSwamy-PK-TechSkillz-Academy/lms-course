dc637cc39d589ce2496f1e9b76aacd51a5ee273d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-12 09:16:48 +0530

    Create LeaveTypesService.cs (#25)

---
+++ src/LMS.Web/Services/LeaveTypesService.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using LMS.Data.Entities;
+using LMS.Persistence;
+using LMS.Web.Controllers;
+using LMS.Web.Models.LeaveTypes;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Web.Services;
+
+public class LeaveTypesService(LMSDbContext lmsDbContext, ILogger<LeaveTypesController> logger, IMapper mapper)
+{
+
+    public async Task<IEnumerable<LeaveTypeReadOnlyVM>> GetAll()
+    {
+        logger.LogInformation("LeaveTypesService::GetAll() visited at {time}", DateTime.Now);
+
+        List<LeaveType> leaveTypes = await lmsDbContext.LeaveTypes.ToListAsync();
+
+        IEnumerable<LeaveTypeReadOnlyVM> leaveTypesVM = mapper.Map<IEnumerable<LeaveTypeReadOnlyVM>>(leaveTypes);
+
+        return leaveTypesVM;
+    }
+
+}
