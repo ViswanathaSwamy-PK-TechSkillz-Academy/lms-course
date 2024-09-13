529dcb0e70fba89e69765cc5f32fc8c17b6f9ab3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-13 11:43:45 +0530

    Update LMSIdentityDbContext.cs

--- src/LMS.IdentityPersistence/LMSIdentityDbContext.cs
+++ src/LMS.IdentityPersistence/LMSIdentityDbContext.cs
@@ -49,5 +49,12 @@ public class LMSIdentityDbContext(DbContextOptions<LMSIdentityDbContext> options
                 EmailConfirmed = true,
             }
         );
+
+        _ = modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = "e9f639de-624f-4a4e-b8bf-2381725462f1",
+                    UserId = "408aa945-3d84-4421-8342-7269ec64d949"
+                });
     }
 }
