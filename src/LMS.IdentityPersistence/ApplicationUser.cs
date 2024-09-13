ef658546b7c01625c461f77f4666d44ac89a36d0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-13 17:58:05 +0530

    Create ApplicationUser.cs

---
+++ src/LMS.IdentityPersistence/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LMS.IdentityPersistence;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public DateOnly DateOfBirth { get; set; }
+}
