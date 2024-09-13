97e1561e4f7d727bc0f2146d69aead2685cf0b78
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-13 12:05:12 +0530

    Identity Initial Version

--- src/LMS.Web/Program.cs
+++ src/LMS.Web/Program.cs
@@ -1,6 +1,14 @@
 using LMS.Web.Extensions;
+using LMS.IdentityPersistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("LMSIdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'LMSIdentityDbContextConnection' not found.");
+
+builder.Services.AddDbContext<LMSIdentityDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<LMSIdentityDbContext>();
 
 // Add services to the container.
 builder.AddConfiguredServices();
