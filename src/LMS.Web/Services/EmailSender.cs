c2f56328d95fd372a72611aa755647ddbd8bb83f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-18 22:45:12 +0530

    Update EmailSender.cs

--- src/LMS.Web/Services/EmailSender.cs
+++ src/LMS.Web/Services/EmailSender.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net.Mail;
 
 namespace LMS.Web.Services;
 
-public class EmailSender : IEmailSender
+public class EmailSender(IConfiguration _configuration) : IEmailSender
 {
-    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
-        throw new NotImplementedException();
+        var fromAddress = _configuration["EmailSettings:DefaultEmailAddress"];
+        var smtpServer = _configuration["EmailSettings:Server"];
+        var smtpPort = Convert.ToInt32(_configuration["EmailSettings:Port"]);
+
+        MailMessage message = new()
+        {
+            From = new MailAddress(fromAddress!),
+            Subject = subject,
+            Body = htmlMessage,
+            IsBodyHtml = true
+        };
+
+        message.To.Add(new MailAddress(email));
+
+        using var client = new SmtpClient(smtpServer, smtpPort);
+
+        await client.SendMailAsync(message);
     }
 }
