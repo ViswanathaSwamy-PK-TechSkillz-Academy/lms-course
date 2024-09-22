bbcddcfd0c8296aa8f3bf2d47315f2a885c28559
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-22 20:34:07 +0530

    Leave Allocations

---
+++ src/LMS.Web/Services/LeaveAllocations/ILeaveAllocationsService.cs
@@ -0,0 +1,14 @@
+﻿using LMS.Data.Entities;
+
+namespace LMS.Web.Services.LeaveAllocations;
+
+public interface ILeaveAllocationsService
+{
+    Task AllocateLeave(string employeeId);
+ 
+    //Task<EmployeeAllocationVM> GetEmployeeAllocations(string? userId);
+    //Task<LeaveAllocationEditVM> GetEmployeeAllocation(int allocationId);
+    //Task<List<EmployeeListVM>> GetEmployees();
+    //Task EditAllocation(LeaveAllocationEditVM allocationEditVm);
+    //Task<LeaveAllocation> GetCurrentAllocation(int leaveTypeId, string employeeId);
+}
---
+++ src/LMS.Web/Services/LeaveAllocations/LeaveAllocationsService.cs
@@ -0,0 +1,10 @@
+﻿
+namespace LMS.Web.Services.LeaveAllocations;
+
+public class LeaveAllocationsService : ILeaveAllocationsService
+{
+    public Task AllocateLeave(string employeeId)
+    {
+        throw new NotImplementedException();
+    }
+}
